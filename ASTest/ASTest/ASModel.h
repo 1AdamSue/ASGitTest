d2591df4983e93c58c2e288f56935ae58bc464dd
1AdamSue <adamsue.11@gmail.com>
2017-03-27 14:59:49 +0800

    add sb file

---
+++ ASTest/ASTest/ASModel.h
@@ -0,0 +1,13 @@
+//
+//  ASModel.h
+//  ASTest
+//
+//  Created by AS on 2017/3/27.
+//  Copyright © 2017年 Adam Sue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASModel : NSObject
+
+@end
---
+++ ASTest/ASTest/ASModel.m
@@ -0,0 +1,13 @@
+//
+//  ASModel.m
+//  ASTest
+//
+//  Created by AS on 2017/3/27.
+//  Copyright © 2017年 Adam Sue. All rights reserved.
+//
+
+#import "ASModel.h"
+
+@implementation ASModel
+
+@end
